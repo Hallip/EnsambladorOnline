29b634dd4553c2b8806f2267b73307a84a530be2
Hallip <juanjmontoya14@gmail.com>
2019-11-19 22:20:54 -0500

    First Generate Method (), clearTable method, change CheckSyntax

--- js/functions.js
+++ js/functions.js
@@ -59,7 +59,6 @@ function remove(){
       var list = document.getElementById("assamblyTable");
       var temp = count;
       temp++;
-      console.log(temp);
       list.removeChild(list.childNodes[temp]);
       count--;
     }
@@ -68,15 +67,16 @@ function remove(){
 function checkSyntax(){
     var memonics = ["LODD","STOD","ADDD","SUBD","JPOS","JZER","JUMP","LOCO","LODL","STOL","ADDL","SUBL","JNEG","JNZE","CALL","PUSHI","POPI","PUSH","POP","RETN","SWAP","INSP","DESP","INPAC","OUTAC","HALT",""]
     var list = [];
+    var error = false;
     for (var i = 0; i < count; i++) {
       id = "mne" + String(i);
       try {
         mne = String(document.getElementById(id).value);
-        console.log(mne);
         if (memonics.includes(mne) || (mne.charAt(0) == '$')){
           if(list.includes(mne) && (mne.charAt(0) == '$')){
             alert("Duplicated variable at: "+ String(i))
             document.getElementById(id).style.color = '#e0555a';
+            error = true;
           }
           else {
             list.push(document.getElementById(id).value);
@@ -84,13 +84,57 @@ function checkSyntax(){
           }
         }
         else {
-          alert("Syntax Error in line: "+ String(i))
+          alert("Syntax Error in line: "+ String(i));
           document.getElementById(id).style.color = '#e0555a';
+          error = true;
+        }
+        if (mne != '' && (String(document.getElementById("mne" + String(i-1)).value) == '')){
+          error = true;
+          alert("Incomplete Syntax at: "+ String(i));
+          document.getElementById(id).style.color = '#e0555a'
         }
 
       } catch (e) {
-        console.log("e");
+        console.log(e);
       }
     }
-    alert("Syntax Generated susfully")
+    if (!error) {
+      alert("Syntax Generated susfully");
+    }
+}
+
+function clearTable(){
+    for (var i = 0;i < count; i++) {
+      var colums = ["hll","lab","mne","ope","mem","mac"];
+      for (var j = 0; j < colums.length; j++) {
+        id = colums[j] + String(i);
+        try {
+          document.getElementById(id).value = '';
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+}
+
+function generate(){
+  var memonics = ["LODD","STOD","ADDD","SUBD","JPOS","JZER","JUMP","LOCO","LODL","STOL","ADDL","SUBL","JNEG","JNZE","CALL","PUSHI","POPI","PUSH","POP","RETN","SWAP","INSP","DESP","INPAC","OUTAC","HALT"]
+  for (var i = 0; i < count; i++) {
+    var id = "mne" + String(i);
+    var hex = "mem" + String(i);
+    try {
+      mne = String(document.getElementById(id).value);
+      if (memonics.includes(mne) || (mne.charAt(0) == '$')) {
+        document.getElementById(hex).value =  toPaddedHexString(i, 2);
+      }
+
+    } catch (e) {
+        console.log(e);
+    }
+  }
+}
+
+function toPaddedHexString(num, len) {
+    str = num.toString(16);
+    return ("0".repeat(len - str.length) + str).toUpperCase();
 }
