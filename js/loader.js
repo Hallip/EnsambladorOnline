b4bb27efa1243b53afe23f9102749b4113829b8c
Juan Montoya <juanjmontoya14@gmail.com>
2019-12-02 12:20:38 -0500

    Preload Page Reorder2

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,8 @@
 window.onload = function (){
+  sleep(100000)
   $('#loader').fadeOut();
   $('body').removeClass('hidden');
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
